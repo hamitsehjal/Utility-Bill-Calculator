f516dc855c784660eada3b8f39b256005ae4c9f2
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-05-23 18:57:53 -0400

    Add project files.

---
+++ Utility-Bill-Calculator/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Utility_Bill_Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
